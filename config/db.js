1f8be6caf23a18eed775bc104b7ce621cc767eee
YENDA VAMSHI KRISHNA <yendavamshikrishna123@gmail.com>
2023-11-07 17:14:25 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,18 +1,18 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-// const sequelize = new Sequelize("iycinternal","iyc_dev","wA42t4gu",{
-//     host : "iycinternal.cuc5uocado2t.ap-south-1.rds.amazonaws.com", 
-//     port : 3306,
-//     dialect : "mysql"
-// });
-
-const sequelize = new Sequelize("testing","root","Vamshi@111047",{
-    host : "localhost", 
+const sequelize = new Sequelize("iycinternal","iyc_dev","wA42t4gu",{
+    host : "iycinternal.cuc5uocado2t.ap-south-1.rds.amazonaws.com", 
     port : 3306,
     dialect : "mysql"
-    // logging: false,
 });
+
+// const sequelize = new Sequelize("testing","root","Vamshi@111047",{
+//     host : "localhost", 
+//     port : 3306,
+//     dialect : "mysql"
+//     // logging: false,
+// });
 
 
 sequelize.authenticate()
